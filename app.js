687fdb168d48b7accf6a59544f5bed694b2231cb
Martin Miret <martin.miret@gmail.com>
2021-02-07 19:08:14 -0300

    1er commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+const mercadopago = require("mercadopago");
 var port = process.env.PORT || 3000
 
 var app = express();
@@ -17,6 +18,49 @@ app.get('/', function (req, res) {
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});
+
+//REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
+mercadopago.configurations.setAccessToken("YOUR_ACCESS_TOKEN"); 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("../../client"));
+
+app.get("/", function (req, res) {
+  res.status(200).sendFile("index.html");
+}); 
+
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+		items: [{
+			title: req.body.description,
+			unit_price: Number(req.body.price),
+			quantity: Number(req.body.quantity),
+		}],
+		back_urls: {
+			"success": "http://localhost:8080/feedback",
+			"failure": "http://localhost:8080/feedback",
+			"pending": "http://localhost:8080/feedback"
+		},
+		auto_return: 'approved',
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({id :response.body.id})
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function(request, response) {
+	 response.json({
+		Payment: request.query.payment_id,
+		Status: request.query.status,
+		MerchantOrder: request.query.merchant_order_id
+	})
 });
 
 app.listen(port);
